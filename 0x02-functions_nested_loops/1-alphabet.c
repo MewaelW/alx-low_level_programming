e7f21782268eabdb5531d943118b64589b6c9606
Mewael <mamewael@gmail.com>
2023-10-10 07:32:09 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - this function prints the alphabet on lowercase 
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	a = 'a';
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
