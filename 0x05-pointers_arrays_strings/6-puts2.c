01880c0f094f0b8b17f41d9a13f8cfa7dd8c0440
Mewael <mamewael@gmail.com>
2023-10-16 07:57:52 -0700

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character on a string
+ *
+ * @str: string to be printed
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int a, size;
+
+	size = 0;
+	while (str[size] != '\0')
+	{
+		size++;
+	}
+	for (a = 0; a <= size; a += 2)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
