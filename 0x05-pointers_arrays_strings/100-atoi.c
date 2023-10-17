66df7aac19409499169053d1407756f68109cb8a
Mewael <mamewael@gmail.com>
2023-10-17 19:33:59 +0300

    100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _atoi - this function converts strings into integers
+ *
+ * @s: string to be converted
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	char w[5];
+	int size;
+
+	w = "Kuzon";
+	size = _strlen(w);
+	return (size);
+}
+
