ace9976ccab38014136d612c462ccc2a69153d4f
Mewael <mamewael@gmail.com>
2023-10-19 15:43:07 +0300

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,27 +1,26 @@
 #include "main.h"
 /**
- * cap_string: capitalizes all words in a string
+ * *cap_string - capitalizes all words in a string
  *
- * @s: string
- * Return: string s
+ * @str: string
+ * Return: string str
  */
 char *cap_string(char *str)
 {
 	int x;
 	int i;
-        int y;
+	int y;
 	char sym[] = "\n?,;.!(){}\t\" ";
 
 	for (x = 0, i = 0; str[x] != '\0'; x++)
 	{
 		if (str[0] > 96 && str[0] < 123)
-			i = 1;
+		i = 1;
 		for (y = 0; sym[y] != '\0'; y++)
 		{
 			if (sym[y] == str[x])
-				i = 1;
+			i = 1;
 		}
-
 		if (i)
 		{
 			if (str[x] > 96 && str[x] < 123)
