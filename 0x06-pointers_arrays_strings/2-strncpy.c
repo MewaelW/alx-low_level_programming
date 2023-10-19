d7d67a040967d6cb6bf9ecc2d5eeb9f18b486e7c
Mewael <mamewael@gmail.com>
2023-10-19 05:26:38 +0300

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string
+ *
+ * @dest: first string 
+ * @src: second string
+ * @n: number of bytes
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len = 0;
+	int i = 0;
+
+	while (dest[len] != '\0')
+		len++;
+	for (i = 0; src[i] != 0 && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	while (n > i)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
