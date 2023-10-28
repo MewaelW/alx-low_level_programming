3ba7173d14b19556840d9a37dbe28a5dbf1b2e1c
Mewael <mamewael@gmail.com>
2023-10-27 19:26:13 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - This function prints the name of a program
+ *
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
