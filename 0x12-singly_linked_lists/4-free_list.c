649cac6dcf308e5246fa0187514828fb8c3f7c33
Mewael <mamewael@gmail.com>
2023-11-17 17:41:00 -0800

    100

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list -This function frees a linked list
+ *
+ * @head: list_t list to be freed
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
