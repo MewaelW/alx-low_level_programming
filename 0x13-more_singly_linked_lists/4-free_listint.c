09a4af699a5e79309ee38746a47673d19966d1b8
Mewael <mamewael@gmail.com>
2023-11-21 17:41:30 -0800

    4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint - this function frees a list
+ * 
+ * @head: Address of the first node
+ **/
+
+void free_listint(listint_t *head)
+{
+	listint_t *p, *pp;
+
+	p = head;
+	while (p != NULL)
+	{
+		pp = p->next;
+		free(p);
+		p = pp;
+	}
+}
